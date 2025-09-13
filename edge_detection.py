51315aef4ab716c14ad633ffc4fd80218f4efff8
Clement Mensah <olitech1010@gmail.com>
2025-09-13 22:57:02 +0000

    text image operators

--- edge_detection.py
+++ edge_detection.py
@@ -19,3 +19,20 @@ def apply_sobel(img):
 
 
 
+# 1. TEXT IMAGE
+
+img1 = cv2.imread("text.jpg", cv2.IMREAD_GRAYSCALE)
+cv2.imshow("Original - Text", img1)
+
+edges1 = cv2.Canny(img1, 50, 150)
+cv2.imshow("Canny 50-150 - Text", edges1)
+
+edges2 = cv2.Canny(img1, 100, 200)
+cv2.imshow("Canny 100-200 - Text", edges2)
+
+edges3 = cv2.Canny(img1, 150, 300)
+cv2.imshow("Canny 150-300 - Text", edges3)
+
+sobel = apply_sobel(img1)
+cv2.imshow("Sobel - Text", sobel)
+
